7b3eadb32df5792a67a8ca55ab1f77752b07b5a8
csPython <cspython@AD25TF04NGF1J-24.local>
2018-12-04 11:55:29 -0500

    comment

---
+++ aliensII04.py
@@ -0,0 +1,35 @@
+#Version 4,
+
+l = int(0)
+w = int(0)
+
+print("The space aliens have invaded Earth!")
+print("   ")
+print("Every week, each space alien gives birth to a new space alien")
+print("   ")
+print("You are the chosen one who must tell us:")
+print("1)how many space aliens landed on Earth?")
+print("   ")
+
+l = int(input("Please give me an answer greater than 0, no decimals."))
+print("  ")
+
+if l > 0:
+    print("how long have the space aliens been on Earth?.")
+    w = int(input("Please give me an answer greater than 0, no decimals."))
+    if w > 0:
+        #for loop+
+
+    
+    
+else:
+    print("Hey you typed something wrong!")
+    print("Now we don't know how many space aliens")
+    print("landed on earth. :(")
+
+
+
+
+
+
+
